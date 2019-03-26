3f17f0bcf8ab1a2d1e01408ef0abf13a72df7f87
Meowch <1311787255@qq.com>
2019-03-26 11:20:44 +0800

    Add Log operator and testLog function

--- src/test/java/LibDL/Tensor/Operator/main.java
+++ src/test/java/LibDL/Tensor/Operator/main.java
@@ -115,6 +115,18 @@ public class main {
     }
 
     @Test
+    public void testLog() {
+        Constant x = new Constant(Nd4j.create(new double[] {0.5, 1, 2.731, 10}), true);
+        Log log = new Log(x);
+        log.forward();
+        assertEquals(log.out, Nd4j.create(new double[] {-0.693147181, 0, 1.00466784, 2.30258509}));
+
+        log.dout = Nd4j.create(new double[] {0.5, 1, 2, -2.5});
+        log.backward();
+        assertEquals(x.dout, Nd4j.create(new double[] {1.64872127, 2.71828183, 7.3890561, 0.0820849986}));
+    }
+
+    @Test
     public void testCrossEntropyLoss() {
         Constant t = new Constant(Nd4j.create(new double[]{0.0, 0, 1, 0, 0, 0, 0, 0, 0, 0}));
         Constant y = new Constant(Nd4j.create(new double[]{0.1, 0.05, 0.6, 0, 0.05, 0.1, 0, 0.1, 0, 0}));
