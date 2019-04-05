ebe7b04b98cdcaed0a6b8396f8d1ad44b1631e7f
Meowch <1311787255@qq.com>
2019-04-05 11:37:22 +0800

    Added LogTest

---
+++ src/test/java/LibDL/Tensor/Operator/LogTest.java
@@ -0,0 +1,39 @@
+package LibDL.Tensor.Operator;
+
+import LibDL.Tensor.Constant;
+import org.junit.Test;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
+import static org.junit.Assert.assertEquals;
+
+public class LogTest {
+    @Test
+    public void testLog() {
+        Constant data_to_backward;
+        Softmax softmax;
+        INDArray target;
+        data_to_backward = new Constant(Nd4j.create(new double[][] {
+                { 1.0,  2.0,  3.0},
+                {-1.0, -2.0,  3.0}
+        }), true);
+        softmax = new Softmax(data_to_backward, 1);
+        Log log = new Log(softmax);
+        log.dout = Nd4j.create(new double[][] {
+                {-2.81521177291870117188, -6.81521177291870117188, -6.81521177291870117188},
+                {-6.04948997497558593750, -6.04948997497558593750,  5.95051002502441406250}
+        });
+        log.forward();
+        target = Nd4j.create(new double[][] {
+                {-2.40760588645935058594, -1.40760588645935058594, -0.40760588645935058594},
+                {-4.02474498748779296875, -5.02474498748779296875, -0.02474498748779296875}
+        });
+        assertEquals(target, log.out);
+        log.backward();
+        target = Nd4j.create(new double[][] {
+                {-1.33460175991058349609, -2.79049634933471679688,  4.12509870529174804688},
+                {-5.93962907791137695312, -6.00907468795776367188, 11.94870281219482421875}
+        });
+        assertEquals(target, data_to_backward.dout);
+    }
+}
