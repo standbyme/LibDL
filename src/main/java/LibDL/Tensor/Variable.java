4b39beffc7d0a773f43d75bfe64e646236aacfe5
sourcerunner <xinfeiyang-2008@163.com>
2019-04-22 14:11:19 +0800

    清除Variable的value属性

--- src/main/java/LibDL/Tensor/Variable.java
+++ src/main/java/LibDL/Tensor/Variable.java
@@ -4,21 +4,17 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 
 public class Variable extends Tensor {
 
-    public final INDArray value;
-
     public Variable(INDArray value) {
         this(value, false);
     }
 
     public Variable(INDArray value, Boolean requires_grad) {
-        this.value = this.data = value;
+        this.data = value;
         this.requires_grad = requires_grad;
-
     }
 
     @Override
     public void backward() {
-
     }
 
     @Override
