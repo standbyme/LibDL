d6897334c34022d05f4b86087b0118b17d19e7dc
sourcerunner <xinfeiyang-2008@163.com>
2019-04-24 14:34:22 +0800

    rewrite part of parameters function to make it clear

--- src/main/java/LibDL/nn/Module.java
+++ src/main/java/LibDL/nn/Module.java
@@ -6,6 +6,7 @@ import LibDL.Tensor.Variable;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 public abstract class Module {
@@ -26,7 +27,41 @@ public abstract class Module {
     public Variable[] parameters() {
         Class<? extends Module> cls = this.getClass();
         Field[] fields = cls.getDeclaredFields();
-        List<Variable> list = new ArrayList<>();
+        List<Variable> result = new ArrayList<>();
+        List<Module> modules = new ArrayList<>();
+        for(Field f: fields) {
+            f.setAccessible(true);
+            Object value = null;
+            try {
+                value = f.get(this);
+            } catch (IllegalAccessException e) {
+                // will not happen.
+                e.printStackTrace();
+            }
+
+            if(value instanceof Variable) {
+                // Add the class's own parameters
+                result.add((Variable)value);
+            }
+            else if(value instanceof Module) {
+                modules.add((Module) value);
+            }
+            else if(value instanceof Module[]) {
+                modules.addAll(Arrays.asList((Module[]) value));
+            }
+        }
+
+        // Add submodules' parameters
+        for(Module module: modules)
+            result.addAll(module.getDeclaredParameters());
+
+        return result.toArray(new Variable[0]);
+    }
+
+    private  Collection<Variable> getDeclaredParameters() {
+        Class<? extends Module> cls = this.getClass();
+        Field[] fields = cls.getDeclaredFields();
+        List<Variable> parameters = new ArrayList<>();
         for(Field f: fields) {
             f.setAccessible(true);
             Object value = null;
@@ -37,19 +72,10 @@ public abstract class Module {
             }
 
             if(value instanceof Variable) {
-                list.add((Variable)value);
-            }
-            else if(value instanceof Module) {
-                list.addAll(Arrays.asList(((Module) value).parameters()));
-            }
-            else if(value instanceof Module[]) {
-                for(Module module: (Module[])value) {
-                    list.addAll(Arrays.asList(module.parameters()));
-                }
+                parameters.add((Variable)value);
             }
         }
-
-        return list.toArray(new Variable[0]);
+        return parameters;
     }
 
 }
