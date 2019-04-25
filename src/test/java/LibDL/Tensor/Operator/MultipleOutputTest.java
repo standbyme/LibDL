c12571f28d6d963601ed70fec4065de59d4a1fcb
joker <joker_xii@outlook.com>
2019-04-26 03:36:30 +0900

    fix pow

--- src/test/java/LibDL/Tensor/Operator/MultipleOutputTest.java
+++ src/test/java/LibDL/Tensor/Operator/MultipleOutputTest.java
@@ -1,4 +1,4 @@
-package LibDL.nn;
+package LibDL.Tensor.Operator;
 
 import LibDL.Tensor.Tensor;
 import LibDL.Tensor.Variable;
@@ -9,10 +9,14 @@ import org.nd4j.linalg.factory.Nd4j;
 public class MultipleOutputTest {
     @Test
     public void test() {
-        Variable a = new Variable(Nd4j.create(new double[]{7}), true);
+        Variable v = new Variable(Nd4j.create(new double[]{7}), true);
+        Tensor a = v.pow(3);
         Tensor b = a.mul(3).add(a.mul(5)).mul(2);
+        // f(v) = ((v^3)*3+(v^3)*5)*2
+        // df(v) = 48v^2dv
         b.grad = Nd4j.create(new double[]{1});
         b.backward();
-        System.out.println(a.grad);
+        System.out.println(v.grad);
+        assert v.grad.getDouble(0) == 2352.00;
     }
 }
