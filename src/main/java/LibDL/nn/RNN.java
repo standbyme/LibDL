bd133be66305bddcf36d9536325f46e9c460675c
sourcerunner <xinfeiyang-2008@163.com>
2019-05-04 23:54:24 +0800

    make RNN as a Module

--- src/main/java/LibDL/nn/RNN.java
+++ src/main/java/LibDL/nn/RNN.java
@@ -1,23 +1,21 @@
 package LibDL.nn;
 
-import LibDL.Tensor.Parameter;
-import LibDL.Tensor.Tensor;
-import LibDL.Tensor.Variable;
+import LibDL.Tensor.*;
 import org.nd4j.linalg.activations.IActivation;
 import org.nd4j.linalg.activations.impl.ActivationTanH;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 
+import java.util.function.Supplier;
+
 import static org.nd4j.linalg.indexing.NDArrayIndex.all;
 import static org.nd4j.linalg.indexing.NDArrayIndex.point;
 
-public class RNN extends Tensor {
+public class RNN extends Module {
 
     // Layer configurations
     private long inputSize;
     private long hiddenSize;
-    // TODO: fix hard-coding here
-    private IActivation activation = new ActivationTanH();
 
     // Layer parameters
     Parameter weight_ih;
@@ -25,15 +23,9 @@ public class RNN extends Tensor {
     Parameter bias_ih;
     Parameter bias_hh;
 
-    // Input
-    Tensor input;
-    Variable h0;
-
-    // Output
-    private Variable hidden;
+    private Variable _h0;
 
     public RNN(int inputSize, int hiddenSize) {
-        super();
         this.inputSize = inputSize;
         this.hiddenSize = hiddenSize;
 
@@ -41,73 +33,110 @@ public class RNN extends Tensor {
         weight_ih = new Parameter(Nd4j.create(inputSize, hiddenSize));
         bias_hh = new Parameter(Nd4j.create(1, hiddenSize));
         bias_ih = new Parameter(Nd4j.create(1, hiddenSize));
+
+        resetParameters();
     }
 
-    public void setInput(Tensor input, Variable h0) {
-        this.input = input;
-        this.h0 = h0;
+    public Tensor forward(Tensor input) {
+        return new RNNImpl(input, _h0);
     }
 
-    public void forward() {
-        data = forwardHelper();
+    public void setH0(Variable h0) {
+        this._h0 = h0;
+    }
+
+    private void resetParameters() {
+        double stdv = 1.0 / Math.sqrt(this.hiddenSize);
+        for(Parameter weight : this.parameters())
+            WeightInit.uniform(weight.data, -stdv, stdv);
     }
 
     @Override
-    public void backward() {
-        backwardHelper(grad);
-        input.backward();
+    public String toString() {
+        return "RNN(inputSize=" + inputSize + ", hiddenSize=" + hiddenSize + ")";
     }
 
+    private class RNNImpl extends OperatorTensor {
+        private Tensor input;
+        private Tensor h0;
 
-    INDArray forwardHelper() {
-        hidden = new Variable(Nd4j.create(input.data.shape()[0], input.data.shape()[1], hiddenSize), true);
-        INDArray prevHidden = h0.data;
-        long times = input.data.size(0);
+        private Tensor hidden;
+        private INDArray input_grad, weight_hh_grad, weight_ih_grad, bias_hh_grad, bias_ih_grad;
 
-        for (long i = 0; i < times; i++) {
-            INDArray currIn = input.data.get(point(i), all(), all());
-            INDArray currOut = hidden.data.get(point(i), all(), all());
-            currOut.assign(currIn.mmul(weight_ih.data.transpose())
-                    .add(prevHidden.mmul(weight_hh.data.transpose()))
-                    .addRowVector(bias_hh.data)
-                    .addRowVector(bias_ih.data)
-            );
-            activation.getActivation(currOut, true);
-            prevHidden = currOut;
+        private IActivation activation = new ActivationTanH();
+
+        RNNImpl(Tensor input, Tensor h0) {
+            this.input = input;
+            this.h0 = h0;
+
+            OperandInfo[] operandInfos = {
+                    new OperandInfo(input, () -> {
+                        backwardHelper(this.grad);
+                        return input_grad;
+                    }),
+                    new OperandInfo(weight_hh, () -> weight_hh_grad),
+                    new OperandInfo(weight_ih, () -> weight_ih_grad),
+                    new OperandInfo(bias_hh, () -> bias_hh_grad),
+                    new OperandInfo(bias_ih, () -> bias_ih_grad),
+            };
+
+            Supplier<INDArray> forward = this::forwardHelper;
+
+            OperatorInfo operatorInfo = new OperatorInfo(operandInfos, forward);
+
+            setOperatorInfo(operatorInfo);
         }
 
-        return hidden.data;
-    }
+        INDArray forwardHelper() {
+            hidden = new Variable(Nd4j.create(input.data.shape()[0], input.data.shape()[1], hiddenSize), true);
+            INDArray prevHidden = h0.data;
+            long times = input.data.size(0);
 
-    void backwardHelper(INDArray epsilon) {
-        input.grad = Nd4j.emptyLike(input.data);
-        weight_hh.grad = Nd4j.zerosLike(weight_hh.data);
-        weight_ih.grad = Nd4j.zerosLike(weight_ih.data);
-        bias_hh.grad = Nd4j.zerosLike(bias_hh.data);
-        bias_ih.grad = Nd4j.zerosLike(bias_ih.data);
+            for (long i = 0; i < times; i++) {
+                INDArray currIn = input.data.get(point(i), all(), all());
+                INDArray currOut = hidden.data.get(point(i), all(), all());
+                currOut.assign(currIn.mmul(weight_ih.data.transpose())
+                        .add(prevHidden.mmul(weight_hh.data.transpose()))
+                        .addRowVector(bias_hh.data)
+                        .addRowVector(bias_ih.data)
+                );
+                activation.getActivation(currOut, true);
+                prevHidden = currOut;
+            }
 
-        INDArray dzNext = null;
-        for (long i = input.data.size(0) - 1; i >= 0; i--) {
-            INDArray epsCurrent = epsilon.get(point(i), all(), all());
-            INDArray hiddenCurrent = hidden.data.get(point(i), all(), all());
-            INDArray inCurrent = input.data.get(point(i), all(), all());
-            INDArray hiddenPrevious = (i == 0) ? h0.data : hidden.data.get(point(i - 1), all(), all());
-            INDArray epsOutCurrent = input.grad.get(point(i), all(), all());
+            return hidden.data;
+        }
 
-            if (dzNext != null)
-                epsCurrent.addi(dzNext.mmul(weight_hh.data));
+        void backwardHelper(INDArray epsilon) {
+            input_grad = Nd4j.emptyLike(input.data);
+            weight_hh_grad = Nd4j.zerosLike(weight_hh.data);
+            weight_ih_grad = Nd4j.zerosLike(weight_ih.data);
+            bias_hh_grad = Nd4j.zerosLike(bias_hh.data);
+            bias_ih_grad = Nd4j.zerosLike(bias_ih.data);
 
-            INDArray dzCurrent = epsCurrent.mul(hiddenCurrent.mul(hiddenCurrent).mul(-1).add(1));
+            INDArray dzNext = null;
+            for (long i = input.data.size(0) - 1; i >= 0; i--) {
+                INDArray epsCurrent = epsilon.get(point(i), all(), all());
+                INDArray hiddenCurrent = hidden.data.get(point(i), all(), all());
+                INDArray inCurrent = input.data.get(point(i), all(), all());
+                INDArray hiddenPrevious = (i == 0) ? h0.data : hidden.data.get(point(i - 1), all(), all());
+                INDArray epsOutCurrent = input_grad.get(point(i), all(), all());
 
-            epsOutCurrent.assign(dzCurrent.mmul(weight_ih.data));
+                if (dzNext != null)
+                    epsCurrent.addi(dzNext.mmul(weight_hh.data));
 
-            weight_ih.grad.addi(dzCurrent.transpose().mmul(inCurrent));
-            weight_hh.grad.addi(dzCurrent.transpose().mmul(hiddenPrevious));
-            bias_hh.grad.addi(dzCurrent.sum(0));
-            bias_ih.grad.addi(dzCurrent.sum(0));
+                INDArray dzCurrent = epsCurrent.mul(hiddenCurrent.mul(hiddenCurrent).mul(-1).add(1));
 
-            dzNext = dzCurrent;
+                epsOutCurrent.assign(dzCurrent.mmul(weight_ih.data));
+
+                weight_ih_grad.addi(dzCurrent.transpose().mmul(inCurrent));
+                weight_hh_grad.addi(dzCurrent.transpose().mmul(hiddenPrevious));
+                bias_hh_grad.addi(dzCurrent.sum(0));
+                bias_ih_grad.addi(dzCurrent.sum(0));
+
+                dzNext = dzCurrent;
+            }
+
         }
     }
-
 }
--- src/test/java/LibDL/nn/RNNTest.java
+++ src/test/java/LibDL/nn/RNNTest.java
@@ -2,7 +2,8 @@ package LibDL.nn;
 
 import LibDL.Tensor.Constant;
 import LibDL.Tensor.Parameter;
-import org.junit.Before;
+import LibDL.Tensor.Tensor;
+import LibDL.Tensor.Variable;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.nd4j.linalg.api.ndarray.INDArray;
@@ -34,9 +35,9 @@ public class RNNTest {
         rnn.bias_ih = new Parameter(Nd4j.create(new double[][]{{-0.1291, -0.2665, -0.0902, 0.3374, 0.2181}}));
     }
 
-    @Before
-    public void setupInput() {
-        INDArray input = Nd4j.create(new double[][][]{
+    @Test
+    public void testRNN() {
+        Variable input = new Variable(Nd4j.create(new double[][][]{
                 {{0.0053, -0.4601, -0.5414},
                         {-0.8522, 0.5896, 0.5357},
                         {-1.4926, 1.1163, -0.1855}},
@@ -44,28 +45,13 @@ public class RNNTest {
                 {{0.2956, 1.4636, 0.6051},
                         {1.0905, 0.5851, -0.4907},
                         {1.0333, -0.2276, -0.1532}}}
-        );
+        ), true);
 
-        INDArray h0 = Nd4j.create(new double[][]{
+        Constant h0 = new Constant(Nd4j.create(new double[][]{
                 {0.4765, 0.3493, -0.8491, 0.7070, 0.6665},
                 {-0.3468, -0.0986, -0.5787, -0.6640, 0.5776},
-                {-0.6707, 0.6514, 0.6557, 0.1384, -0.9628}});
+                {-0.6707, 0.6514, 0.6557, 0.1384, -0.9628}}));
 
-        rnn.setInput(new Constant(input), new Constant(h0));
-
-        rnn.grad = Nd4j.create(new double[][][]{{
-                {1.1837e+00, 2.8680e-02, 5.9473e-01, -6.3787e-01, -9.8196e-01},
-                {-1.5350e+00, 7.6189e-01, 1.3230e+00, -4.2295e-01, 5.3208e-01},
-                {-1.4129e+00, -2.3161e+00, -2.5905e-02, 1.8038e+00, -7.0832e-01}},
-
-                {{-1.7072e+00, -3.1917e+00, 1.1541e+00, -1.9135e+00, 2.3066e-01},
-                        {-3.6231e-03, 3.9820e-01, 4.9735e-01, -1.5231e+00, -2.8920e-03},
-                        {-4.8295e-01, -2.3305e+00, -1.2397e+00, 1.6851e+00, 1.8875e-01}}}
-        );
-    }
-
-    @Test
-    public void testForward() {
         INDArray output = Nd4j.create(new double[][][]{
                 {{-0.7930, -0.4247, -0.1755, 0.3985, 0.2128},
                         {-0.1581, -0.6048, -0.4263, 0.2726, 0.1788},
@@ -75,14 +61,24 @@ public class RNNTest {
                         {-0.3656, -0.2373, -0.2324, -0.1165, 0.1217},
                         {-0.5072, -0.5500, -0.3009, -0.1026, 0.4916}}});
 
-        rnn.forward();
+        rnn.setH0(h0);
+        Tensor result = rnn.forward(input);
 
-        assert rnn.data.equalsWithEps(output, 1e-3);
+        assert result.data.equalsWithEps(output, 1e-3);
 
-    }
 
-    @Test
-    public void testBackward() {
+        result.grad = Nd4j.create(new double[][][]{{
+                {1.1837e+00, 2.8680e-02, 5.9473e-01, -6.3787e-01, -9.8196e-01},
+                {-1.5350e+00, 7.6189e-01, 1.3230e+00, -4.2295e-01, 5.3208e-01},
+                {-1.4129e+00, -2.3161e+00, -2.5905e-02, 1.8038e+00, -7.0832e-01}},
+
+                {{-1.7072e+00, -3.1917e+00, 1.1541e+00, -1.9135e+00, 2.3066e-01},
+                        {-3.6231e-03, 3.9820e-01, 4.9735e-01, -1.5231e+00, -2.8920e-03},
+                        {-4.8295e-01, -2.3305e+00, -1.2397e+00, 1.6851e+00, 1.8875e-01}}}
+        );
+        result.backward();
+
+
         INDArray inputGradient = Nd4j.create(new double[][][]{
                 {{-0.0317, 0.5793, -0.3325},
                         {0.2363, -0.2091, -0.7278},
@@ -105,11 +101,7 @@ public class RNNTest {
                 {-0.9795, -0.1644, 1.5444, -0.5361, -1.5196}});
         INDArray biasGradient = Nd4j.create(new double[]{-2.8803, -5.3557, 1.3418, -2.3531, -0.6663});
 
-        rnn.forward();
-
-        rnn.backward();
-
-        assert rnn.input.grad.equalsWithEps(inputGradient, 1e-3);
+        assert input.grad.equalsWithEps(inputGradient, 1e-3);
         assert rnn.weight_ih.grad.equalsWithEps(weightGradient_ih, 1e-3);
         assert rnn.weight_hh.grad.equalsWithEps(weightGradient_hh, 1e-3);
         assert rnn.bias_ih.grad.equalsWithEps(biasGradient, 1e-3);
