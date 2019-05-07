235383a1eb00d331c77dbf810cd89b3d94dcbd34
joker <joker_xii@outlook.com>
2019-05-08 00:10:27 +0900

    fix some bugs

--- src/main/java/LibDL/nn/GRU.java
+++ src/main/java/LibDL/nn/GRU.java
@@ -8,12 +8,10 @@ public class GRU extends RNNAuto {
     }
 
     @Override
-    protected void forward_loop(Tensor input, Tensor[] outList, Tensor prevHidden, int seqLen) {
-
+    protected void rnn_impl(Tensor input, Tensor[] outList, Tensor prevHidden, int seqLen) {
         for (int i = 0; i < seqLen; i++) {
             Tensor currIn = input.get(i);
-            Tensor r = calculate_gate(currIn, prevHidden, gro_weight_ih, gro_weight_hh, bias_hh, bias_ih, null);
-
+            Tensor r = calculate_gate(currIn, prevHidden, gro_weight_ih, gro_weight_hh, gro_bias_hh, gro_bias_ih, null);
 
             Tensor currOut = calculate_gate(currIn, prevHidden, weight_ih, weight_hh, bias_hh, bias_ih, r);
 
@@ -22,9 +20,7 @@ public class GRU extends RNNAuto {
             Tensor u = Functional.sigmoid(
                     calculate_gate(currIn, prevHidden, gu_weight_ih, gu_weight_hh, gu_bias_hh, gu_bias_ih, null)
             );
-            outList[i] = u.mul(currOut)
-                    .add(Tensor.ones(u.sizes())
-                            .sub(u).mul(prevHidden));
+            outList[i] = u.mul(currOut).add(Tensor.ones(u.sizes()).sub(u).mul(prevHidden));
             prevHidden = currOut;
         }
     }
