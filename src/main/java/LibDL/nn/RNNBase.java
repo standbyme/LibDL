85d7535faeca34465c2c5617bcaab20b777e0689
joker <joker_xii@outlook.com>
2019-05-13 22:07:41 +0900

    Update RNNBase.java

--- src/main/java/LibDL/nn/RNNBase.java
+++ src/main/java/LibDL/nn/RNNBase.java
@@ -11,33 +11,35 @@ import org.nd4j.linalg.indexing.NDArrayIndex;
 
 
 abstract public class RNNBase extends Module {
-    public static final int WEIGHT_IH = 0, WEIGHT_HH = 1, BIAS_IH = 2, BIAS_HH = 3;
-
     // Layer configurations
-    protected long inputSize;
-    protected long hiddenSize;
-    protected long numLayers;
+    public long inputSize;
+    public long hiddenSize;
+    public long numLayers;
 
     //use ReLU instead of tanh
-    protected boolean relu;
+    public boolean relu;
 
-    //dropout
-    protected boolean dropout;
-    protected double dropout_p;
+    //doDropout
+    protected boolean doDropout;
+    public double dropout;
 
     //use bias
-    protected boolean bias;
+    public boolean bias;
 
     //not implemented
-    protected boolean batch_first;
-    protected boolean bid;
+    public boolean batch_first;
+    public boolean bidirectional;
 
-    public enum RNNType {
+    public static final int WEIGHT_IH = 0, WEIGHT_HH = 1, BIAS_IH = 2, BIAS_HH = 3;
+    public static final int PARAM_I = 0, PARAM_F = 1, PARAM_G = 2, PARAM_O = 3;
+    public static final int PARAM_R = 0, PARAM_Z = 1, PARAM_N = 2;
+
+    enum RNNType {
         TYPE_RNN(1),
-        TYPE_LSTM(4),
-        TYPE_GRU(3);
+        TYPE_GRU(3),
+        TYPE_LSTM(4);
 
-        private final int value;
+       private final int value;
 
         RNNType(int value) {
             this.value = value;
@@ -48,18 +50,35 @@ abstract public class RNNBase extends Module {
         }
     }
 
-    protected int PARAM_I = 0, PARAM_F = 1, PARAM_G = 2, PARAM_O = 3;
-    protected int PARAM_R = 0, PARAM_Z = 1, PARAM_N = 2;
+    public RNNType rnn_type;
 
     public Parameter[] weight_ih;
     public Parameter[] weight_hh;
     public Parameter[] bias_ih;
     public Parameter[] bias_hh;
 
-    protected RNNType rnn_type;
 
     protected int pm(int layer, int param_type) {
-        return rnn_type.value * layer + param_type;
+        return rnn_type.gateSize() * layer + param_type;
+    }
+
+
+    public Parameter[] getParamsByType(int which) {
+        switch (which) {
+            case WEIGHT_HH:
+                return (weight_hh);
+            case WEIGHT_IH:
+                return (weight_hh);
+            case BIAS_HH:
+                return (bias_hh);
+            case BIAS_IH:
+                return (bias_ih);
+        }
+        return null;
+    }
+
+    public Parameter getParameter(int which, int param_type, int layer) {
+        return getParamsByType(which)[pm(layer, param_type)];
     }
 
     protected void init_param() {
@@ -105,9 +124,10 @@ abstract public class RNNBase extends Module {
         this.batch_first = batch_first;
         this.rnn_type = type;
         this.bias = bias;
+        this.bidirectional = bidirectional;
 
-        this.dropout = dropout != 0;
-        this.dropout_p = dropout;
+        this.doDropout = dropout != 0;
+        this.dropout = dropout;
 
         this.h_n = new Tensor[numLayers];
         init_param();
@@ -135,7 +155,7 @@ abstract public class RNNBase extends Module {
         for (int layer = 0; layer < numLayers; layer++) {
             outList = rnn_impl(nxt, outList, prevHidden, seqLen, c0, layer);
             output = new Concat(0, outList).reshape(seqLen, batchSize, hiddenSize);
-            if (dropout) nxt = Functional.dropout(output, dropout_p);
+            if (doDropout) nxt = Functional.dropout(output, dropout);
 
         }
 
@@ -182,24 +202,9 @@ abstract public class RNNBase extends Module {
     }
 
 
-    public void setParam(int param_type, INDArray... params) {
-        Parameter[] paramList = null;
+    public void setParam(Parameter[] params_to_set, INDArray... params) {
         for (int currLayer = 0; currLayer < numLayers; currLayer++) {
             INDArray param = params[currLayer];
-            switch (param_type) {
-                case WEIGHT_HH:
-                    paramList = weight_hh;
-                    break;
-                case WEIGHT_IH:
-                    paramList = weight_ih;
-                    break;
-                case BIAS_HH:
-                    paramList = bias_hh;
-                    break;
-                case BIAS_IH:
-                    paramList = bias_ih;
-            }
-
 
             INDArrayIndex[] indices = new INDArrayIndex[param.rank()];
             for (int i = 1; i < indices.length; i++) {
@@ -208,11 +213,15 @@ abstract public class RNNBase extends Module {
 
             for (int gate_type = 0; gate_type < rnn_type.gateSize(); gate_type++) {
                 indices[0] = NDArrayIndex.interval(gate_type * hiddenSize, gate_type * hiddenSize + hiddenSize);
-                assert paramList != null;
-                paramList[pm(currLayer, gate_type)].data = param.get(indices);
+                assert params_to_set[pm(currLayer, gate_type)] != null;
+                params_to_set[pm(currLayer, gate_type)].data = param.get(indices);
             }
         }
     }
 
+    public void setParam(int param_type, INDArray... params) {
+        setParam(getParamsByType(param_type), params);
+
+    }
 
 }
