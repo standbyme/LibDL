8687acee6611f2b5d4c4b0df1db678f7153160d0
StandbyMe <stand-by-me@mail.com>
2019-05-29 16:02:31 +0800

    Use Tensor instead of Nd4jTensor

--- src/main/java/LibDL/ld.java
+++ src/main/java/LibDL/ld.java
@@ -8,19 +8,19 @@ import org.nd4j.linalg.factory.Nd4j;
 public class ld {
     static SameDiff sd = SameDiff.create();
 
-    public static @NotNull Nd4jTensor tensor(@NotNull int[] values) {
+    public static @NotNull Tensor tensor(@NotNull int[] values) {
         return new Nd4jTensor(sd.var(Nd4j.create(values)));
     }
 
-    public static @NotNull Nd4jTensor randn(@NotNull int[] size) {
+    public static @NotNull Tensor randn(@NotNull int[] size) {
         return new Nd4jTensor(sd.var(Nd4j.randn(size)));
     }
 
-    public static @NotNull Nd4jTensor randn(int size) {
+    public static @NotNull Tensor randn(int size) {
         return new Nd4jTensor(sd.var(Nd4j.randn(size)));
     }
 
-    public static @NotNull Nd4jTensor addmm(@NotNull Tensor b, @NotNull Tensor input, @NotNull Tensor w) {
+    public static @NotNull Tensor addmm(@NotNull Tensor b, @NotNull Tensor input, @NotNull Tensor w) {
         if (b instanceof Nd4jTensor && input instanceof Nd4jTensor && w instanceof Nd4jTensor)
             return new Nd4jTensor(sd.var(Nd4j.empty()));
         throw new IllegalArgumentException();
